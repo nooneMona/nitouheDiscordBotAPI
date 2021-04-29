f90ab2b9bf1fe3ac183ea29bb01a33fdb809e24a
Noone <shobonaoc@gmail.com>
2021-04-29 23:59:51 +0900

    一応簡単なパスワード付きで。

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Headers,
+  HttpStatus,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
+class HelloDto {
+  id: number;
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +20,21 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  postHello(
+    @Headers('Authorization') auth: string,
+    @Body() helloDto: HelloDto,
+    @Res() res: Response,
+  ): string {
+    if (auth !== process.env.AUTH_PASS) {
+      res.status(HttpStatus.UNAUTHORIZED).send();
+      return;
+    }
+    console.log('auth');
+    res.json({
+      id: helloDto.id + 1,
+    });
   }
 }
